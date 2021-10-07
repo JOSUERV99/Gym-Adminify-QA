b765e87626be8650b3d9c7700a3f417bee20700f
Esteban Madrigal <ememadrigal.em@gmail.com>
2021-10-07 10:49:32 -0600

    Init GetDatesByDayTestCase

--- gym_adminfy_django/gymActivities/tests.py
+++ gym_adminfy_django/gymActivities/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
-
+from views import AllActivities
 # Create your tests here.
+class GetDatesByDayTestCase(TestCase):
+
+    def test_animals_can_speak(self):
+        """Animals that can speak are correctly identified"""
+        all_act = AllActivities() 
+        all_act.getDatesByDay()
+        self.assertEqual(lion.speak(), 'The lion says "roar"')
+        self.assertEqual(cat.speak(), 'The cat says "meow"')
