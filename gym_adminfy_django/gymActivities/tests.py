9b6e550227e94b9f7a8ff3ddce6e84f4ef4b64d2
Kevin Fallas <kf.ma@estudiantec.cr>
2021-10-07 12:58:06 -0600

    Update tests.py

--- gym_adminfy_django/gymActivities/tests.py
+++ gym_adminfy_django/gymActivities/tests.py
@@ -1,11 +1,31 @@
 from django.test import TestCase
-from views import AllActivities
-# Create your tests here.
-class GetDatesByDayTestCase(TestCase):
 
-    def test_animals_can_speak(self):
-        """Animals that can speak are correctly identified"""
-        all_act = AllActivities() 
-        all_act.getDatesByDay()
-        self.assertEqual(lion.speak(), 'The lion says "roar"')
-        self.assertEqual(cat.speak(), 'The cat says "meow"')
+# from .models import Activity
+# from .views import AllActivities
+
+import datetime
+
+# Create your tests here.
+# class GetDatesByDayTestCase(TestCase):
+
+#     def test_animals_can_speak(self):
+#         """Animals that can speak are correctly identified"""
+#         # all_act = AllActivities() 
+#         # all_act.getDatesByDay()
+#         # self.assertEqual(lion.speak(), 'The lion says "roar"')
+#         self.assertEqual('The cat says "roar"', 'The cat says "meow"')
+        
+class CheckOverlap(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        #Activity.objects.create(dayofweek = 5,startime = datetime.time(10, 30, 00),endtime = datetime.time(11, 30, 00))
+        pass
+    
+    def dia_fecha_negativos(self):
+        #all_act = AllActivities()
+        startTime = datetime.time(10, 30, 00)
+        endTime = datetime.time(11, 30, 00)
+        day = 5
+        # print(all_act.checkOverlap(startTime, endTime,day))
+        # self.assertIs(all_act.checkOverlap(startTime, endTime,day), True)
