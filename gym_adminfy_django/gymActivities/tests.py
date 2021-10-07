8a7813764128f96cd44ad8eed259388461f553f9
Esteban Madrigal <ememadrigal.em@gmail.com>
2021-10-07 12:58:15 -0600

    4 tests done

--- gym_adminfy_django/gymActivities/tests.py
+++ gym_adminfy_django/gymActivities/tests.py
@@ -1,11 +1,33 @@
-from django.test import TestCase
-from views import AllActivities
+from calendar import IllegalMonthError
+import datetime
+from django.test import SimpleTestCase
+from .views import AllActivities
+from .models import Activity
 # Create your tests here.
-class GetDatesByDayTestCase(TestCase):
+class GetDatesByDayTestCase(SimpleTestCase):
 
-    def test_animals_can_speak(self):
-        """Animals that can speak are correctly identified"""
+    def test_non_existent_month(self):
         all_act = AllActivities() 
-        all_act.getDatesByDay()
-        self.assertEqual(lion.speak(), 'The lion says "roar"')
-        self.assertEqual(cat.speak(), 'The cat says "meow"')
+        try:
+            test = all_act.getDatesByDay(1,13,2021)
+        except IllegalMonthError:
+            self.fail("There is not error handling with non-existen month")
+    
+    def test_non_existent_day(self):
+        all_act = AllActivities()     
+        try:
+            test = all_act.getDatesByDay(10,10,2021)
+        except IllegalMonthError:
+            self.fail("There is not error handling with non-existen day")
+
+    def test_negative_year(self):
+        all_act = AllActivities()    
+        try:
+            test = all_act.getDatesByDay(1,10,-2020)
+        except ValueError:
+            self.fail("There is not error handling with negative year")
+    
+    def test_leap_year_and_day_29(self):
+        all_act = AllActivities()    
+        test = all_act.getDatesByDay(6,2,2020)
+        self.assertEqual(test,[datetime.date(2020, 2, 1), datetime.date(2020, 2, 8), datetime.date(2020, 2, 15), datetime.date(2020, 2, 22), datetime.date(2020, 2, 29)])        
