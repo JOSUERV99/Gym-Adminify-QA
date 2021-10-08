61ca767ceb4d0e1e12c6346ba4de264717fcbc2a
Esteban Madrigal <ememadrigal.em@gmail.com>
2021-10-07 18:25:45 -0600

    Update tests.py

--- gym_adminfy_django/gymActivities/tests.py
+++ gym_adminfy_django/gymActivities/tests.py
@@ -1,39 +1,36 @@
-from calendar import IllegalMonthError
+from calendar import IllegalMonthError, month
 import datetime
 from django.test import SimpleTestCase
 from .views import AllActivities
 
 # Create your tests here.
 class GetDatesByDayTestCase(SimpleTestCase):
-
+    
+    def setUp(self):
+        self.all_act = AllActivities() 
+    
     def test_non_existent_month(self):
-        all_act = AllActivities() 
         try:
-            test = all_act.getDatesByDay(1,13,2021)
+            self.all_act.getDatesByDay(1,13,2021)
         except IllegalMonthError:
             self.fail("There is not error handling with non-existen month")
     
-    def test_non_existent_day(self):
-        all_act = AllActivities()     
+    def test_non_existent_day(self):   
         try:
-            test = all_act.getDatesByDay(10,10,2021)
+            self.all_act.getDatesByDay(10,10,2021)
         except IllegalMonthError:
             self.fail("There is not error handling with non-existen day")
 
-    def test_negative_year(self):
-        all_act = AllActivities()    
+    def test_negative_year(self):  
         try:
-            test = all_act.getDatesByDay(1,10,-2020)
+            self.all_act.getDatesByDay(1,10,-2020)
         except ValueError:
             self.fail("There is not error handling with negative year")
     
     def test_leap_year_and_day_29(self):
-        all_act = AllActivities()    
-        test = all_act.getDatesByDay(6,2,2020)
+        test = self.all_act.getDatesByDay(6,2,2020)
         self.assertEqual(test,[datetime.date(2020, 2, 1), datetime.date(2020, 2, 8), datetime.date(2020, 2, 15), datetime.date(2020, 2, 22), datetime.date(2020, 2, 29)])        
     
-    def test_current_date(self):
-        all_act = AllActivities()    
-        test = all_act.getDatesByDay(4,10,2021)
-        print(test)
+    def test_current_date(self):  
+        test = self.all_act.getDatesByDay(4,10,2021)
         self.assertEqual(test,[datetime.date(2021, 10, 7), datetime.date(2021, 10, 14), datetime.date(2021, 10, 21), datetime.date(2021, 10, 28)])        
