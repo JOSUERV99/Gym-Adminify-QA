c5c542f07d996c8944e4dd82d6f0e129ac25e596
Esteban Madrigal <ememadrigal.em@gmail.com>
2021-10-10 22:58:03 -0600

    ValidateHours test case init

---
+++ gym_adminify_vue/tests/unit/Activities.spec.js
@@ -0,0 +1,32 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Activities from '../../src/views/Activities.vue'
+import Vuex from 'vuex'
+
+const store = new Vuex.Store({
+                              state: {
+                                permissions: []
+                              },
+                              mutations: {
+                                setIsLoading(state,status){
+                                  state.isLoading = status
+                                }
+                              },
+                            })
+describe('Activities.vue', () => {
+  it('renders a message and responds correctly to user input', () => {
+    const wrapper = shallowMount(Activities, {
+      data() {
+        return {
+          activityStartTime_new: '10:30',
+          activityEndTime_new: '10:30'
+        }
+      },
+      global: {
+      mocks: {
+        $store: store,
+      }
+    } })
+
+    expect(wrapper.vm.validateHours()).toBe(true)
+  })
+})
