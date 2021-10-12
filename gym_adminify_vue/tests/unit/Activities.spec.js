67b074970fc6aff79f8519462814e314f6c662df
Eduardo <edumadrim@gmail.com>
2021-10-11 22:29:01 -0600

    validateHours() tests

--- gym_adminify_vue/tests/unit/Activities.spec.js
+++ gym_adminify_vue/tests/unit/Activities.spec.js
@@ -3,30 +3,83 @@ import Activities from '../../src/views/Activities.vue'
 import Vuex from 'vuex'
 
 const store = new Vuex.Store({
-                              state: {
-                                permissions: []
-                              },
-                              mutations: {
-                                setIsLoading(state,status){
-                                  state.isLoading = status
-                                }
-                              },
-                            })
-describe('Activities.vue', () => {
-  it('renders a message and responds correctly to user input', () => {
-    const wrapper = shallowMount(Activities, {
+  state: {
+    permissions: []
+  },
+  mutations: {
+    setIsLoading(state, status) {
+      state.isLoading = status
+    }
+  },
+})
+
+// * Test Suite for Activities.vue -> validateHours()
+describe('Activities.vue -> validateHours() tests: ', () => {
+
+  // Object that contains component
+  let wrapper = null
+
+  // Executes before test suite
+  beforeAll(() => {
+    // Component with default values
+    wrapper = shallowMount(Activities, {
       data() {
         return {
-          activityStartTime_new: '10:30',
-          activityEndTime_new: '10:30'
+          activityStartTime_new: '',
+          activityEndTime_new: ''
         }
       },
       global: {
-      mocks: {
-        $store: store,
+        mocks: {
+          $store: store,
+        }
       }
-    } })
-
-    expect(wrapper.vm.validateHours()).toBe(true)
+    })
   })
+
+  // Executes after test suite
+  afterAll(() => {
+    wrapper.destroy()
+  })
+
+  // Case 1: Inputted start-time is after inputted end-time
+  it('Start-time after end-time', () => {
+    wrapper.setData({
+      activityStartTime_new: '11:30',
+      activityEndTime_new: '10:30'
+    })
+
+    expect(wrapper.vm.validateHours()).toBe(false)
+  })
+
+  // Case 2: Inputted start-time and end-time have the same time value
+  it('Same start-time and end-time', () => {
+    wrapper.setData({
+      activityStartTime_new: '10:30',
+      activityEndTime_new: '10:30'
+    })
+
+    expect(wrapper.vm.validateHours()).toBe(false)
+  })
+
+  // Case 3: Inputted start-time and end-time are both empty strings
+  it('Empty start-time and end-time', () => {
+    wrapper.setData({
+      activityStartTime_new: '',
+      activityEndTime_new: ''
+    })
+
+    expect(wrapper.vm.validateHours()).toBe(false)
+  })
+
+  // Case 4: Inputted start-time and end-time are both out of range
+  it('Start-time and end-time out of range', () => {
+    wrapper.setData({
+      activityStartTime_new: '10:65',
+      activityEndTime_new: '25:65'
+    })
+    
+    expect(wrapper.vm.validateHours()).toBe(false)
+  })
+
 })
